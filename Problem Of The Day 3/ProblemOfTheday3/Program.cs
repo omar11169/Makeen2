9c83f077faeeb3d133af388e0b426454d5e0d868
Omar al balushi <omar-albalushi@outlook.com>
2025-04-22 16:39:45 +0400

    Add files via upload

---
+++ Problem Of The Day 3/ProblemOfTheday3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+                {1, 2, 3, },
+                {4, 5, 6, },
+                {9, 8 ,9 }
+            };
+            int m = matrix.GetLength(0);
+            int first = 0;
+            int second = 0;
+            
+            for (int i = 0; i < m; i++)
+            {
+                first += matrix[i,i];
+                second += matrix[i,m -i - 1];
+            }
+            int result =  first - second ;
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
